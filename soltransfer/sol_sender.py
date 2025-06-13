8b589c99c1d1a56ffb416592d5b2eea332519aa5
Nischay Hegde <nishegde>
2025-06-13 04:37:29 -0500

    final stretch

--- soltransfer/sol_sender.py
+++
@@ -1,9 +0,0 @@
-from termcolor import cprint
-from pprint import pprint
-import base58
-import json
-import time
-
-from solana.rpc.api import Client
-from solana.rpc.api import Keypair
-from solana.rpc.api import RPCException
